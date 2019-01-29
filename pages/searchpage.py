f192d9eb585883280400a45c2ae6511704ee811d
wangbo <820585630@qq.com>
2019-01-29 14:46:33 +0800

    search-case 细节调整

--- pages/searchpage.py
+++ pages/searchpage.py
@@ -1,27 +1,19 @@
-from Base.base import Base
-from robot.api.deco import keyword
-from services.CommonLibrary import CommonLibrary
+from pages.basepage import BasePage
 
-class Searchpage(Base):
-    cl = CommonLibrary()
+
+class Searchpage(BasePage):
     search_box = ('class', 'search-box')  # 搜索框
-    search_input = ('id', 'searchInput')  # 搜索词输入框
-    search_result = ('class', 'item-container') #搜索结果内容容器
+    search_input = ('id', 'searchInput', 0)  # 搜索词输入框
+    search_result = ('class', 'tabs-nav', 0)  # 搜索结果内容容器
 
     def click_search_box(self):
-        self.click(self.findElement(self.search_box))
+        self.element.click(self.search_input)
 
     def input_searchWord(self, searchword):
-        #输入搜索词
-        self.send_keys(self.findElement(self.search_input), searchword)
-        #点击回车
-        self.enter(self.findElement(self.search_input))
+        # 输入搜索词
+        self.element.send_keys(self.search_input, searchword)
+        # 点击回车
+        self.element.enter(self.search_input)
+
     def check_searchResult(self):
-        self.findElement(self.search_result)
-
-
-
-
-
-
-
+        self.element.is_element_exist(self.search_result)
