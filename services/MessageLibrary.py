97894b1af92f163e2c332b027beeeec1c9653859
tcas8227 <m15548485220@163.com>
2019-03-12 18:42:38 +0800

    other center

--- services/MessageLibrary.py
+++ services/MessageLibrary.py
@@ -1,12 +1,17 @@
-# from robot.api.deco import keyword
-# from pages.messagepage import MessagePage
-#
-#
-# class MessageLibrary(object):
-#
-#     def __init__(self):
-#         self.msg = MessagePage()
-#
-#     @keyword
-#     def should_be_message_page(self):
-#         self.msg.is_message_page()
+from robot.api.deco import keyword
+from pages.messagepage import MessagePage
+
+
+class MessageLibrary(object):
+
+    def __init__(self):
+        self.msg = MessagePage()
+
+    @keyword
+    def should_be_message_page(self):
+        self.msg.is_message_page()
+
+    @keyword
+    def hover_and_click_seeallmessage(self):
+        self.msg.hover_message_headerbar()
+        self.msg.click_see_all_message()
