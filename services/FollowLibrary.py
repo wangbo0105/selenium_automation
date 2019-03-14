b7eef980496f49ba4e896a8f4a6dae515172b0ed
wangbo <820585630@qq.com>
2019-03-14 10:51:14 +0800

    follow-case
    Summary: follow-case
    
    Test Plan: feature case
    
    Reviewers: zhougn
    
    Differential Revision: https://cr.veervr.club/D5514

--- services/FollowLibrary.py
+++ services/FollowLibrary.py
@@ -1,35 +1,64 @@
 from robot.api.deco import keyword
 from pages.follow import Follow
-from pages.basepage import BasePage
-from services.PhotoLibrary import PhotoLibrary
-from services.CommonLibrary import CommonLibrary
 from services.PersonalLibrary import PersonalLibrary
 
 
 class FollowLibrary(object):
 
     def __init__(self):
-        self.common = CommonLibrary()
         self.follow = Follow()
-        self.photo = PhotoLibrary()
-        self.base = BasePage()
         self.personal = PersonalLibrary()
 
     @keyword
-    def go_followed_page(self):
+    def go_following_page(self):
+        self.personal.go_personal_center()
         self.follow.go_following_page()
 
     @keyword
-    def follow_creater(self):
-        self.common.go_page('photo')
-        self.photo.photo_click_item('photo_content')
+    def check_current_page_is_following_page(self):
+        self.follow.check_following_page()
+
+    @keyword
+    def go_follower_page(self):
+        self.personal.go_personal_center()
+        self.follow.go_follower_page()
+
+    @keyword
+    def check_current_page_is_follower_page(self):
+        self.follow.check_follower_page()
+
+    @keyword
+    def click_follow_btn(self):
         self.follow.click_follow_btn()
 
     @keyword
-    def Check_the_creator_is_focused_on_success(self):
-        self.follow.check_follow_in_detail_page()
+    def click_followed_btn(self):
+        self.follow.click_followed_button()
+
+    @keyword
+    def check_follow_state_in_detail_page(self, type):
+        self.follow.check_follow_state_in_detail_page(type)
+
+    @keyword
+    def check_follow_state_in_following_page(self, type):
+        self.follow.check_follow_state_in_follow_page(type)
+
+    @keyword
+    def check_followed_num(self, num):
+        self.personal.go_personal_center()
+        self.follow.check_followed_num_in_personal_center(num)
         self.follow.go_following_page()
-        self.follow.check_follow_in_follow_page()
+        self.follow.check_followed_num_in_following_page(num)
+
+    @keyword
+    def choose_follow_operation(self, type):
+        self.follow.select_follow_type(type)
+
+    @keyword
+    def follow_creater_in_creater_home_page(self):
+        self.go_follower_page()
+        self.follow.go_follower_home_page()
+        self.click_follow_btn()
 
     @keyword
     def clear_following(self):
--- smoke/follow/TC_follow.robot
+++ smoke/follow/TC_follow.robot
@@ -1,18 +1,28 @@
 *** Settings ***
 Documentation    follow smoke test
+Suite Setup  Run Keywords  load veer  AND  login  ${usrname}  ${password}
 Test Setup  load veer
 Suite Teardown  close my browser
 Library  services.CommonLibrary
 Library  services.FollowLibrary
 Library  services.LoginLibrary
+Library  services.HomeLibrary
 Resource  ../../testdata/userdata.robot
 
 *** Test Cases ***
-follow creater
-    Given login  ${usrname}   ${password}
-    When follow creater
-    Then check the creator is focused on success
+follow creater in content detail page
+    Given click feeds content item  精选图片
+    When click follow btn
+    Then check follow state in detail page  ${True}
+    Then check followed num  1
+    And check follow state in following page  ${True}
+
+following page :cancel follw alert select unfollow
+    Given go following page
+    Then click followed btn
+    When choose follow operation  ${True}
+    Then check follow state in following page  ${False}
 
 clear following
-    Then go followed page
-    And clear following
+    Given go following page
+    Then clear following
