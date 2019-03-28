c69e05a1b2256d5472e3ad8ca638a59ae1e658f9
Zhang Juxuan <juxuanz@gmail.com>
2019-03-28 19:18:47 +0800

    upload
    Summary: upload one photo demo
    
    Test Plan: local test
    
    Reviewers: zhougn
    
    Differential Revision: https://cr.veervr.club/D5652

--- pages/uploadpage.py
+++ pages/uploadpage.py
@@ -1,10 +1,26 @@
-# from pages.basepage import BasePage
-#
-#
-# class UploadPage(BasePage):
-#     upload_tab = ('class', 'upload-btn', 0)  # 导航栏-上传
-#     upload_title = ('class', 'upload-title', 0)  # 上传页面-标题
-#
-#     def is_upload_page(self):
-#         self.window.is_text_in_url('upload')
-#         self.element.is_element_exist(self.upload_title)
+from pages.basepage import BasePage
+import time
+
+
+
+class UploadPage(BasePage):
+    upload_tab = ('class', 'upload-btn', 0)  # 导航栏-上传
+    upload_title = ('class', 'upload-title', 0)  # 上传页面-标题
+    # uoplodBtn = ('xpath','//input[@id="fileSelector"]',0)
+    uoplodBtn = ('id','fileSelector',0)
+    
+    photoCanvas = ('class', 'photo-preview', 0)
+
+    def click_upload(self):
+        self.element.click(self.upload_tab)
+
+    def is_upload_page(self):
+        self.window.is_text_in_url('upload')
+        self.element.is_element_exist(self.upload_title)
+
+    def upload_one_photo(self):
+        self.element.send_keys(self.uoplodBtn,"/Users/cc/testfile/test_photos/ gear/gear_road/IMG_0647.JPG")
+
+    def is_photo_Upload_page(self):
+        time.sleep(4)
+        self.element.is_element_exist(self.photoCanvas)
--- services/UploadLibrary.py
+++ services/UploadLibrary.py
@@ -1,11 +1,25 @@
-# from robot.api.deco import keyword
-# from pages.uploadpage import UploadPage
-#
-#
-# class UploadLibrary(object):
-#     def __init__(self):
-#         self.upload = UploadPage()
-#
-#     @keyword
-#     def should_be_upload_page(self):
-#         self.upload.is_upload_page()
+from robot.api.deco import keyword
+from pages.uploadpage import UploadPage
+
+
+class UploadLibrary(object):
+    def __init__(self):
+        self.upload = UploadPage()
+
+    @keyword
+    def select_upload(self):
+        self.upload.click_upload()
+        self.upload.is_upload_page()
+
+    @keyword
+    def should_be_upload_page(self):
+        self.upload.is_upload_page()
+
+    @keyword
+    def upload_one_photo(self):
+        self.upload.upload_one_photo()
+
+    @keyword
+    def is_upload_page(self):
+        self.upload.is_photo_Upload_page()
+        
